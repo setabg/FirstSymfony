2481d7ef37e95c8dc6ac6ce837e143a9a87818c6
setabg <cveti@reklama.bg>
2021-09-27 06:06:21 +0300

    Templates

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return new Response('<h1>Welcome FirstSymfony</h1>');
+    }
+
+    /**
+     * @Route("/custom/{name?}", name="custom")
+     * @param Request $request
+     * @return Response
+     */
+    public function custom(Request $request){
+        $name=$request->get('name');
+        return new Response('<h1>Welcome '. $name.' </h1>');
+    }
+}
