49f3b93fdf7f738448525d4455c4364b824569f7
setabg <cveti@reklama.bg>
2021-09-30 17:33:04 +0300

    register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,18 +3,37 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/registration", name="registration")
+     * @Route("/register", name="register")
      */
-    public function index(): Response
-    {
+    public function register(){
+
+        $form=$this->createFormBuilder()
+            ->add('username')
+            ->add('password', RepeatedType::class, [
+                  'type' => PasswordType::class,
+                    'required' => true,
+                    'first_options'  => ['label' => 'Password'],
+                    'second_options' => ['label' => 'Confirm Password']
+             ])
+            ->add('register', SubmitType::class, [
+                'attr'=>[
+                    'class'=>'btn btn-success float-right'
+                ]
+            ])
+            ->getForm()
+            ;
+
         return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
+               'form'=>$form->createView()
         ]);
     }
 }
